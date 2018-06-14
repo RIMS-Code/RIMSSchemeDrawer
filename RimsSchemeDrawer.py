835ce79eebd18badeb829290cd090f620df36120
trappitsch <trappitsch1@6hearts.llnl.gov>
2018-06-14 16:38:28 -0700

    New features
    Can now select cm-1 or nm
    Ground state manifold can be selected to, so we can draw Ti, U, etc.
    properly

--- RimsSchemeDrawer.py
+++ RimsSchemeDrawer.py
@@ -38,7 +38,7 @@ class RSD:
         master.title("RIMS Scheme Drawer")
 
         # Geometry and stuff
-        rows, cols = 8, 4
+        rows, cols = 8, 5
         BW, BH, pad = 120, 30, 15
         winsize = ((1.5 + cols / 2) * pad + cols * BW,
                    (1.5 + rows / 2) * pad + rows * BH,
@@ -55,19 +55,15 @@ class RSD:
         self.font_bold = tkFont.Font(size=11, weight=tkFont.BOLD)
         self.font_ital = tkFont.Font(size=11, slant=tkFont.ITALIC)
 
-        # Labels for the given steps (nm)
-        self.step_title_label = tk.Label(master, text='Level', anchor=tk.W, font=self.font_bold)
-        self.step_title_label.place(x=self.x(2), y=self.y(1), width=BW, height=BH)
-        self.step1_label = tk.Label(master, text='1st step (nm):', anchor=tk.W, font=self.font_std)
-        self.step2_label = tk.Label(master, text='2nd step (nm):', anchor=tk.W, font=self.font_std)
-        self.step3_label = tk.Label(master, text='3rd step (nm):', anchor=tk.W, font=self.font_std)
-        self.step4_label = tk.Label(master, text='4th step (nm):', anchor=tk.W, font=self.font_std)
-        self.step5_label = tk.Label(master, text='5th step (nm):', anchor=tk.W, font=self.font_std)
-        self.step1_label.place(x=self.x(1), y=self.y(3), width=BW, height=BH)
-        self.step2_label.place(x=self.x(1), y=self.y(4), width=BW, height=BH)
-        self.step3_label.place(x=self.x(1), y=self.y(5), width=BW, height=BH)
-        self.step4_label.place(x=self.x(1), y=self.y(6), width=BW, height=BH)
-        self.step5_label.place(x=self.x(1), y=self.y(7), width=BW, height=BH)
+        # radiobutton for nm or cm-1
+        self.unitvar = tk.IntVar()
+        self.unit_selector_nm = tk.Radiobutton(master, text='nm', anchor=tk.W, font=self.font_std,
+                                               variable=self.unitvar, value=1, command=self.setsteplabels)
+        self.unit_selector_nm.place(x=self.x(1), y=self.y(1), width=BW/2, height=BH)
+        self.unit_selector_cm = tk.Radiobutton(master, text='cm^-1', anchor=tk.E, font=self.font_std,
+                                               variable=self.unitvar, value=2, command=self.setsteplabels)
+        self.unit_selector_cm.place(x=self.x(1.5), y=self.y(1), width=BW/2, height=BH)
+        self.unit_selector_cm.select()
 
         # box for Ground state
         self.ground_state_label = tk.Label(master, text='Ground state (cm^-1)', anchor=tk.W, font=self.font_std)
@@ -111,33 +107,100 @@ class RSD:
         self.term5.place(x=self.x(3), y=self.y(7), width=BW, height=BH)
         self.termip.place(x=self.x(3), y=self.y(8), width=BW, height=BH)
 
+        # Part of GS manifold
+        self.mf_label = tk.Label(master, text='GS Manifold?', anchor=tk.W, font=self.font_bold)
+        self.mf_label.place(x=self.x(4), y=self.y(1), width=BW, height=BH)
+        self.mf1_var = tk.IntVar()
+        self.mf1_box = tk.Checkbutton(master, text='Low-lying state?', justify=tk.LEFT,
+                                      variable=self.mf1_var, font=self.font_std, command=self.setsteplabels)
+        self.mf1_box.place(x=self.x(4), y=self.y(3), width=BW, height=BH)
+        self.mf2_var = tk.IntVar()
+        self.mf2_box = tk.Checkbutton(master, text='Low-lying state?', justify=tk.LEFT,
+                                      variable=self.mf2_var, font=self.font_std, command=self.setsteplabels)
+        self.mf2_box.place(x=self.x(4), y=self.y(4), width=BW, height=BH)
+        self.mf3_var = tk.IntVar()
+        self.mf3_box = tk.Checkbutton(master, text='Low-lying state?', justify=tk.LEFT,
+                                      variable=self.mf3_var, font=self.font_std, command=self.setsteplabels)
+        self.mf3_box.place(x=self.x(4), y=self.y(5), width=BW, height=BH)
+        self.mf4_var = tk.IntVar()
+        self.mf4_box = tk.Checkbutton(master, text='Low-lying state?', justify=tk.LEFT,
+                                      variable=self.mf4_var, font=self.font_std, command=self.setsteplabels)
+        self.mf4_box.place(x=self.x(4), y=self.y(6), width=BW, height=BH)
+        self.mf5_var = tk.IntVar()
+        self.mf5_box = tk.Checkbutton(master, text='Low-lying state?', justify=tk.LEFT,
+                                      variable=self.mf5_var, font=self.font_std, command=self.setsteplabels)
+        self.mf5_box.place(x=self.x(4), y=self.y(7), width=BW, height=BH)
+
         # Title
         self.title_label = tk.Label(master, text='Plot Title', anchor=tk.W, font=self.font_bold)
-        self.title_label.place(x=self.x(4), y=self.y(1), width=BW, height=BH)
+        self.title_label.place(x=self.x(5), y=self.y(1), width=BW, height=BH)
         self.title_entry = tk.Entry(master)
-        self.title_entry.place(x=self.x(4), y=self.y(2), width=BW, height=BH)
+        self.title_entry.place(x=self.x(5), y=self.y(2), width=BW, height=BH)
 
         # Buttons
         self.button_plot = tk.Button(master, text='Plot', font=self.font_std, command=self.plotter)
         self.button_save = tk.Button(master, text='Save', font=self.font_std, command=self.save)
+        # test button
+        # self.button_test = tk.Button(master, text='Test', font=self.font_std, command=self.test)
         self.button_help = tk.Button(master, text='Help', font=self.font_std, command=self.help)
         self.button_quit = tk.Button(master, text='Quit', font=self.font_std, command=self.quit)
-        self.button_plot.place(x=self.x(4), y=self.y(3), width=BW, height=BH)
-        self.button_save.place(x=self.x(4), y=self.y(4), width=BW, height=BH)
-        self.button_help.place(x=self.x(4), y=self.y(7), width=BW, height=BH)
-        self.button_quit.place(x=self.x(4), y=self.y(8), width=BW, height=BH)
+        self.button_plot.place(x=self.x(5), y=self.y(3), width=BW, height=BH)
+        self.button_save.place(x=self.x(5), y=self.y(4), width=BW, height=BH)
+        # self.button_test.place(x=self.x(5), y=self.y(6), width=BW, height=BH)
+        self.button_help.place(x=self.x(5), y=self.y(7), width=BW, height=BH)
+        self.button_quit.place(x=self.x(5), y=self.y(8), width=BW, height=BH)
 
         # Options
         self.opt_linebreak_st = tk.IntVar()
         self.opt_linebreak = tk.Checkbutton(master, text='Linebreaks?', justify=tk.LEFT,
                                             variable=self.opt_linebreak_st, font=self.font_std)
-        self.opt_linebreak.place(x=self.x(4), y=self.y(5), width=BW, height=BH)
+        self.opt_linebreak.place(x=self.x(5), y=self.y(5), width=BW, height=BH)
+
+        # set step labels
+        self.setsteplabels()
 
         # fixme: temporary insert, remove later
-        # self.step1_lambda.insert(tk.END, '273.448')
-        # self.step2_lambda.insert(tk.END, '450.326')
-        # self.step3_lambda.insert(tk.END, '820.357')
-        # self.ip_value.insert(tk.END, '60003.')
+        # self.ground_state_value.insert(tk.END, '0.')
+        # self.step1_lambda.insert(tk.END, '200.')
+        # self.mf1_box.select()
+        # self.step2_lambda.insert(tk.END, '19387')
+        # self.step3_lambda.insert(tk.END, '37020')
+        # self.ip_value.insert(tk.END, '36000.')
+
+    def setsteplabels(self):
+        # set unit
+        if self.unitvar.get() == 1:
+            unit = 'nm'
+        else:
+            unit = 'cm^-1'
+        # Labels for the given steps (nm)
+        self.step_title_label = tk.Label(self.master, text='Level', anchor=tk.W, font=self.font_bold)
+        self.step_title_label.place(x=self.x(2), y=self.y(1), width=self.BW, height=self.BH)
+        if self.mf1_var.get():
+            self.step1_label = tk.Label(self.master, text='Low-lying st (cm^-1):', anchor=tk.W, font=self.font_std)
+        else:
+            self.step1_label = tk.Label(self.master, text='1st step (' + unit + '):', anchor=tk.W, font=self.font_std)
+        if self.mf2_var.get():
+            self.step2_label = tk.Label(self.master, text='Low-lying st (cm^-1):', anchor=tk.W, font=self.font_std)
+        else:
+            self.step2_label = tk.Label(self.master, text='2nd step (' + unit + '):', anchor=tk.W, font=self.font_std)
+        if self.mf3_var.get():
+            self.step3_label = tk.Label(self.master, text='Low-lying st (cm^-1):', anchor=tk.W, font=self.font_std)
+        else:
+            self.step3_label = tk.Label(self.master, text='3rd step (' + unit + '):', anchor=tk.W, font=self.font_std)
+        if self.mf4_var.get():
+            self.step4_label = tk.Label(self.master, text='Low-lying st (cm^-1):', anchor=tk.W, font=self.font_std)
+        else:
+            self.step4_label = tk.Label(self.master, text='4th step (' + unit + '):', anchor=tk.W, font=self.font_std)
+        if self.mf5_var.get():
+            self.step5_label = tk.Label(self.master, text='Low-lying st (cm^-1):', anchor=tk.W, font=self.font_std)
+        else:
+            self.step5_label = tk.Label(self.master, text='5th step (' + unit + '):', anchor=tk.W, font=self.font_std)
+        self.step1_label.place(x=self.x(1), y=self.y(3), width=self.BW, height=self.BH)
+        self.step2_label.place(x=self.x(1), y=self.y(4), width=self.BW, height=self.BH)
+        self.step3_label.place(x=self.x(1), y=self.y(5), width=self.BW, height=self.BH)
+        self.step4_label.place(x=self.x(1), y=self.y(6), width=self.BW, height=self.BH)
+        self.step5_label.place(x=self.x(1), y=self.y(7), width=self.BW, height=self.BH)
 
     def x(self, col):
         # Calculates x coordinate of gui element
@@ -157,18 +220,67 @@ class RSD:
     def plotter(self, fname=None):
         # textpad
         textpad = 0.4
+        # percentage to increase for manifold
+        mfld_yinc = 0.04   # in # of ipvalue
+        #
+        firstarrowxmfl = 1.
         # let's first get the wavelengths that we want
         lambda1 = self.step1_lambda.get()
         lambda2 = self.step2_lambda.get()
         lambda3 = self.step3_lambda.get()
         lambda4 = self.step4_lambda.get()
         lambda5 = self.step5_lambda.get()
-        lambda_steps = [lambda1, lambda2, lambda3, lambda4, lambda5]
+
+        # now only add lambdas that are NOT low lying states!
+        lambda_steps = []
+        if not self.mf1_var.get():
+            lambda_steps.append(lambda1)
+        if not self.mf2_var.get():
+            lambda_steps.append(lambda2)
+        if not self.mf3_var.get():
+            lambda_steps.append(lambda3)
+        if not self.mf4_var.get():
+            lambda_steps.append(lambda4)
+        if not self.mf5_var.get():
+            lambda_steps.append(lambda5)
+
         try:
             ipvalue = float(self.ip_value.get())
         except ValueError:
             messagebox.showerror('Enter IP', 'Please enter an ionization potential as a number and try again.')
             return
+
+        # get the term symbols that were entered
+        term_symb_entered = []
+        if not self.mf1_var.get():
+            term_symb_entered.append(self.term1.get())
+        if not self.mf2_var.get():
+            term_symb_entered.append(self.term2.get())
+        if not self.mf3_var.get():
+            term_symb_entered.append(self.term3.get())
+        if not self.mf4_var.get():
+            term_symb_entered.append(self.term4.get())
+        if not self.mf5_var.get():
+            term_symb_entered.append(self.term5.get())
+
+        # get ground state wavenumber
+        if self.ground_state_value.get() is '':
+            wavenumber_gs = 0
+        else:
+            wavenumber_gs = float(self.ground_state_value.get())
+
+        # now go through the lambda steps and transform into actual wavelengths if not already
+        if self.unitvar.get() == 2:
+            lambda_steps_temp = []
+            for it in range(len(lambda_steps)):
+                if lambda_steps[it] != '':
+                    if it == 0:
+                        lambda_steps_temp.append(1.e7 / (float(lambda_steps[it]) - float(wavenumber_gs)))
+                    else:
+                        lambda_steps_temp.append(1.e7 / (float(lambda_steps[it]) - float(lambda_steps[it-1])))
+            # write lambda_steps back
+            lambda_steps = list(lambda_steps_temp)
+
         # make lambda_steps into an np.array
         tmp = []
         for it in lambda_steps:
@@ -178,19 +290,24 @@ class RSD:
                 break
         lambda_steps = np.array(tmp)
 
-        # get the term symbols that were entered
-        term_symb_entered = []
-        term_symb_entered.append(self.term1.get())
-        term_symb_entered.append(self.term2.get())
-        term_symb_entered.append(self.term3.get())
-        term_symb_entered.append(self.term4.get())
-        term_symb_entered.append(self.term5.get())
-
-        # get ground state wavenumber
-        if self.ground_state_value.get() is '':
-            wavenumber_gs = 0
-        else:
-            wavenumber_gs = float(self.ground_state_value.get())
+        # get more ground states if available
+        wavenumber_es = []
+        term_symb_es = []
+        if self.mf1_var.get():
+            wavenumber_es.append(self.step1_lambda.get())
+            term_symb_es.append(self.term1.get())
+        if self.mf2_var.get():
+            wavenumber_es.append(self.step2_lambda.get())
+            term_symb_es.append(self.term2.get())
+        if self.mf3_var.get():
+            wavenumber_es.append(self.step3_lambda.get())
+            term_symb_es.append(self.term3.get())
+        if self.mf4_var.get():
+            wavenumber_es.append(self.step4_lambda.get())
+            term_symb_es.append(self.term4.get())
+        if self.mf5_var.get():
+            wavenumber_es.append(self.step5_lambda.get())
+            term_symb_es.append(self.term5.get())
 
         # create wavenumber array
         wavenumber_steps = 1. / lambda_steps * 1e7
@@ -213,8 +330,11 @@ class RSD:
 
         # create term symbol string for direct usage in plotting
         term_symb = []
+        term_symb_es_formatted = []
         for it in range(len(wavenumber_steps)):
             term_symb.append(term_to_string(term_symb_entered[it]))
+        for it in range(len(term_symb_es)):
+            term_symb_es_formatted.append(term_to_string(term_symb_es[it]))
         # get term symbol for ip and gs
         term_symb_ip = term_to_string(self.termip.get())
         term_symb_gs = term_to_string(self.termgs.get())
@@ -249,6 +369,10 @@ class RSD:
         for it in transition_steps:
             if it < ipvalue:
                 a.hlines(it, xmin=0, xmax=10)
+        # Lines for manifold groundstater
+        for it in range(len(wavenumber_es)):
+            a.hlines(mfld_yinc*ipvalue*(1+it), xmin=1.5*it+2.3, xmax=1.5*it+3.7,
+                     linestyle='dashed')
 
         # Draw the vertical lines for every transition and IP, unless transition is above IP (shade area there)
         for it in transition_steps:
@@ -268,8 +392,8 @@ class RSD:
             levelstr = str(round(wavenumber_gs, 3)) + '$\,$cm$^{-1}$'
         else:
             levelstr = str(round(wavenumber_gs, 3)) + '$\,$cm$^{-1}$' + lbreak + term_symb_gs
-
         a.text(10. - textpad, float(self.ground_state_value.get()), levelstr, color='k', ha='right', va='bottom')
+
         for it in range(len(lambda_steps)):
             if lambda_steps[it] >= 700:
                 col = self.colir
@@ -283,8 +407,12 @@ class RSD:
             xval += deltax
             wstp = wavenumber_steps[it]
             tstp = transition_steps[it]
-            a.arrow(xval, yval_bott, 0, wstp, width=0.2, fc=col, ec=col, length_includes_head=True,
-                    head_width=0.6, head_length=totwavenumber_photons/30.)
+            if it == 0 and len(wavenumber_es) > 0:
+                a.arrow(firstarrowxmfl, yval_bott, 0, wstp, width=0.2, fc=col, ec=col, length_includes_head=True,
+                        head_width=0.6, head_length=totwavenumber_photons/30.)
+            else:
+                a.arrow(xval, yval_bott, 0, wstp, width=0.2, fc=col, ec=col, length_includes_head=True,
+                        head_width=0.6, head_length=totwavenumber_photons / 30.)
 
             # alignment of labels
             if xval <= 5.:
@@ -300,7 +428,11 @@ class RSD:
 
             # wavelength text
             lambdastr = str(round(lambda_steps[it], 3)) + '$\,$nm'
-            a.text(xloc_lambda, tstp - wstp/2., lambdastr, color=col, ha=halignlam)
+            if it == 0 and len(wavenumber_es) > 0:
+                a.text(firstarrowxmfl + textpad, tstp - wstp/2., lambdastr, color=col, ha=halignlam, va='center',
+                       rotation=90)
+            else:
+                a.text(xval + textpad, tstp - wstp/2., lambdastr, color=col, ha=halignlam, va='center', rotation=90)
 
             # level text
             if term_symb[it] is None:
@@ -311,6 +443,42 @@ class RSD:
 
             # update yval_bott
             yval_bott = transition_steps[it]
+
+        # create ground state lambda step array
+        lambda_step_es = []
+        for it in range(len(wavenumber_es)):
+            lambda_step_es.append(1.e7 / (1.e7/lambda_steps[0] - (float(wavenumber_es[it]) - float(wavenumber_gs))))
+
+        # now go through low lying excited states
+        for it in range(len(wavenumber_es)):
+            if lambda_step_es[it] >= 700:
+                col = self.colir
+            elif 500. < lambda_step_es[it] < 700.:
+                col = self.colpump
+            elif 350. < lambda_step_es[it] <= 500.:
+                col = self.coluv
+            else:
+                col = self.colfuv
+            # xvalue for arrow
+            xval = firstarrowxmfl + 1.5 + it * 1.5
+            yval = mfld_yinc*ipvalue*(1+it)
+            wstp = float(wavenumber_steps[0]) - yval
+            print wstp
+            print wavenumber_es[it]
+            a.arrow(xval, yval, 0, wstp, width=0.2, fc=col, ec=col, length_includes_head=True,
+                    head_width=0.6, head_length=totwavenumber_photons / 30.)
+
+            # wavelength text
+            lambdastr = str(round(lambda_step_es[it], 3)) + '$\,$nm'
+            a.text(xval + textpad, yval + wstp/2., lambdastr, color=col, ha='left', va='center', rotation=90)
+
+            # level text
+            if term_symb_es_formatted[it] is None:
+                levelstr = str(round(float(wavenumber_es[it]), 3)) + '$\,$cm$^{-1}$'
+            else:
+                # NO LINEBREAK HERE ON THESE LINES!
+                levelstr = str(round(float(wavenumber_es[it]), 3)) + '$\,$cm$^{-1}$, ' + term_symb_es_formatted[it]
+            a.text(xval + 0.5, yval, levelstr, color='k', ha='left', va='bottom')
 
         # Title:
         if self.title_entry is not '':
@@ -344,6 +512,9 @@ class RSD:
         else:
             f.savefig(fname)
 
+    def test(self):
+        print self.unitvar.get()
+
     def help(self):
         messagebox.showinfo('Help', 'This program can be used to automatically generate RIMS scheme figures that look '
                                     'pretty. You can either plot them and look at them on screen, or save them in a '
@@ -361,7 +532,7 @@ class RSD:
                                     '    the level and the term symbol.\n\n'
                                     'Questions?  trappitsch1@llnl.gov\n'
                                     'Complaints? isselhardt1@llnl.gov\n\n'
-                                    'Version: 20180111')
+                                    'Version: 20180614')
 
     def quit(self):
         self.master.destroy()
@@ -376,6 +547,16 @@ def term_to_string(tstr):
     """
     if tstr == '':
         return None
+
+    # some exceptionslike AI and IP
+    if tstr == 'IP':
+        return 'IP'
+    if tstr == 'AI':
+        return 'AI'
+    if tstr == 'Rydberg':
+        return 'Rydberg'
+    if tstr == 'Ryd':
+        return 'Ryd'
 
     # if there is an equal sign in there, leave it as is
     if tstr.find('=') != -1:
