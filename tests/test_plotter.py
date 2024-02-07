e3ccad9d155c1bde12cd0b3f2a39dcb11ea73b39
Reto Trappitsch <reto@galactic-forensics.space>
2024-02-07 08:35:20 +0100

    fix test for CI

--- tests/test_plotter.py
+++ tests/test_plotter.py
@@ -4,11 +4,13 @@ from rimsschemedrawer.plotter import Plotter
 from rimsschemedrawer import utils as ut
 
 
-def test_plotter(data_path):
+def test_plotter(data_path, tmp_path):
     """Run through the plotter with a simple scheme, assert no errors."""
     fin = data_path.joinpath("ti.json")
     data = ut.json_reader(fin)
 
-    # fixme: replace with temp path
+    fname = tmp_path.joinpath("test.pdf")
     fig = Plotter(data)
-    fig.savefig("/home/reto/Desktop/test.pdf")
+    fig.savefig(fname)
+
+    assert fname.exists()
